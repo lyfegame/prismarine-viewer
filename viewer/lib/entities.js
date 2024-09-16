8003d67cfec0699e24a3e18f9a6195fb1432f9b5
Nic Becker <nic@altera.al>
2024-09-16 11:04:18 -0700

    fix headless

--- viewer/lib/entities.js
+++ viewer/lib/entities.js
@@ -3,6 +3,7 @@ const TWEEN = require('@tweenjs/tween.js')
 
 const Entity = require('./entity/Entity')
 const { dispose3 } = require('./dispose')
+const { createCanvas } = require('canvas')
 
 function getEntityMesh (entity, scene) {
   if (entity.name) {
@@ -10,9 +11,7 @@ function getEntityMesh (entity, scene) {
       const e = new Entity('1.16.4', entity.name, scene)
 
       if (entity.username !== undefined) {
-        const canvas = document.createElement('canvas')
-        canvas.width = 500
-        canvas.height = 100
+        const canvas = createCanvas(500, 100)
 
         const ctx = canvas.getContext('2d')
         ctx.font = '50pt Arial'
